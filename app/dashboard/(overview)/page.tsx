aaa42d2d5f5524e20ba9690bc6db76543c22954e
chengcheng.ni <chengcheng.ni@boardfun.cn>
2024-03-26 10:06:53 +0800

    feat：16章节init

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,7 +6,10 @@ import { lusitana } from '@/app/ui/fonts';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
- 
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+	title: 'Home',
+};
 export default async function Page() {
 	// const [cardData] = await Promise.all([
 		// fetchRevenue(),
